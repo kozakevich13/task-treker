688f5104467e1e01a05e056e1820b0fa176b7f83
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-11-30 12:14:57 +0200

    add function sortedTasks

--- screens/TaskList.js
+++ screens/TaskList.js
@@ -3,10 +3,16 @@ import { View, FlatList } from 'react-native';
 import Task from './Task';
 
 const TaskList = ({ tasks, toggleTask }) => {
+
+  const sortedTasks = [...tasks].sort((a, b) => {
+    if (a.completed && !b.completed) return 1;
+    if (!a.completed && b.completed) return -1;
+    return 0;
+  });
   return (
     <View>
       <FlatList
-        data={tasks}
+        data={sortedTasks}
         keyExtractor={(task) => task.id.toString()}
         renderItem={({ item }) => (
           <Task task={item} toggleTask={toggleTask} />
