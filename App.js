6adeda957663e4a7cc80d935f62e3db45865f897
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-12-19 22:36:13 +0200

    add local store for main tasks

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Main from "./screens/Main"
-import DailyTasksScreen from "./screens/DailyTasksScreen"
-
-
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Main from "./screens/Main";
+import DailyTasksScreen from "./screens/DailyTasksScreen";
 
 const Stack = createStackNavigator();
 const App = () => {
- 
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        {/* <View style={styles.container}> */}
         <Stack.Screen name="Home" component={Main} />
         <Stack.Screen name="DailyTasks" component={DailyTasksScreen} />
-        {/* </View> */}
-    </Stack.Navigator>
-  </NavigationContainer>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 const styles = StyleSheet.create({
@@ -29,6 +23,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
 
 export default App;
--- screens/Main.js
+++ screens/Main.js
@@ -1,23 +1,30 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import TaskList from "./TaskList";
 
-
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
-import TaskList from './TaskList';
-
-
-const Main = ({navigation }) => {
-
-  const [taskText, setTaskText] = useState('');
+const Main = ({ navigation }) => {
+  const [taskText, setTaskText] = useState("");
   const [tasks, setTasks] = useState([]);
   const [completedCount, setCompletedCount] = useState(0);
 
-  const addDailyTaskToMain = useCallback((newTask) => {
-    setTasks([...tasks, newTask]);
-  }, [tasks]);
+  const addDailyTaskToMain = useCallback(
+    (newTask) => {
+      setTasks([...tasks, newTask]);
+    },
+    [tasks]
+  );
 
   useEffect(() => {
+    // Завантаження завдань з локального сховища при монтажі компонента
+    loadTasks();
+  }, []);
+
+  useEffect(() => {
+    // Збереження завдань в локальному сховищі при зміні стану tasks
+    saveTasks();
     updateCompletedCount();
-  }, [tasks]); // Запускати updateCompletedCount при зміні стану tasks
+  }, [tasks]);
 
   const addTask = () => {
     if (taskText) {
@@ -28,16 +35,14 @@ const Main = ({navigation }) => {
       };
 
       setTasks([...tasks, newTask]);
-      setTaskText('');
+      setTaskText("");
     }
   };
 
   const toggleTask = (taskId) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
-        task.id === taskId
-          ? { ...task, completed: !task.completed }
-          : task
+        task.id === taskId ? { ...task, completed: !task.completed } : task
       )
     );
   };
@@ -47,43 +52,58 @@ const Main = ({navigation }) => {
     setCompletedCount(count);
   };
 
+  const loadTasks = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem("tasks");
+      if (storedTasks) {
+        setTasks(JSON.parse(storedTasks));
+      }
+    } catch (error) {
+      console.error("Error loading tasks:", error);
+    }
+  };
 
+  const saveTasks = async () => {
+    try {
+      await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
+    } catch (error) {
+      console.error("Error saving tasks:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-    <Text>main page!</Text>
-    <Button
+      <Text>Main page!</Text>
+      <Button
         title="Go to Daily Tasks"
         onPress={() =>
-          navigation.navigate('DailyTasks', {
+          navigation.navigate("DailyTasks", {
             addDailyTaskToMain: addDailyTaskToMain,
           })
         }
-        
       />
-    <TextInput
-      style={styles.input}
-      placeholder="Enter task"
-      value={taskText}
-      onChangeText={(text) => setTaskText(text)}
-    />
-    <Button title="Add Task" onPress={addTask} />
-    <Text>Total Completed Tasks: {completedCount}</Text>
-    <TaskList tasks={tasks} toggleTask={toggleTask} />
-  </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter task"
+        value={taskText}
+        onChangeText={(text) => setTaskText(text)}
+      />
+      <Button title="Add Task" onPress={addTask} />
+      <Text>Total Completed Tasks: {completedCount}</Text>
+      <TaskList tasks={tasks} toggleTask={toggleTask} />
+    </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 8,
     marginTop: 20,
