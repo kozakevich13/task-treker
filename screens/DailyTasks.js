67d1e5695815321ea8016774ed052bf4cd58b4fb
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-12-21 18:48:50 +0200

    add functional for dailytasks

--- screens/DailyTasks.js
+++ screens/DailyTasks.js
@@ -1,12 +1,96 @@
 // DailyTasks.js
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const DailyTasks = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const addTask = () => {
+    let newTask = {
+      id: Date.now().toString(),
+      text: task,
+      completed: false,
+      repeat: false,
+      repeat_num: 0,
+    };
+    if (task.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setTask("");
+      saveTasks([...tasks, newTask]);
+    }
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+    saveTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
+  const saveTasks = async (tasks) => {
+    try {
+      const jsonTasks = JSON.stringify(tasks);
+      await AsyncStorage.setItem("dailyTasks", jsonTasks);
+    } catch (error) {
+      console.error("Error saving tasks: ", error);
+    }
+  };
+
+  const loadTasks = async () => {
+    try {
+      const jsonTasks = await AsyncStorage.getItem("dailyTasks");
+      if (jsonTasks) {
+        setTasks(JSON.parse(jsonTasks));
+      }
+    } catch (error) {
+      console.error("Error loading tasks: ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Daily Tasks</Text>
-      {/* Додайте ваші елементи сторінки тут */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter task"
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <Button title="Add Task" onPress={addTask} />
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.taskItem}>
+            <Text
+              style={{
+                textDecorationLine: item.completed ? "line-through" : "none",
+              }}
+            >
+              {item.text}
+            </Text>
+            <Text
+              style={{
+                textDecorationLine: item.completed ? "line-through" : "none",
+              }}
+            >
+              {item.repeat_num}
+            </Text>
+            <Button title="Remove" onPress={() => removeTask(item.id)} />
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -20,6 +104,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  taskItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 8,
   },
 });
 
